96779db6b38b03a782b34ab6884401d741c1d3aa
Faunu <Tegucigalpa>
2021-11-12 00:24:02 +0300

    clean up

--- ascii_game/colored_text.py
+++ ascii_game/colored_text.py
@@ -8,19 +8,19 @@ class _ColorCodes(NamedTuple):
 
 
 class ANSIColor(_ColorCodes, Enum):
-    BLACK = _ColorCodes('\033[30m', '\033[40m')
-    RED = _ColorCodes('\033[31m', '\033[41m')
-    GREEN = _ColorCodes('\033[32m', '\033[42m')
-    YELLOW = _ColorCodes('\033[33m', '\033[43m')
-    BLUE = _ColorCodes('\033[34m', '\033[44m')
-    MAGENTA = _ColorCodes('\033[35m', '\033[45m')
-    CYAN = _ColorCodes('\033[36m', '\033[46m')
-    WHITE = _ColorCodes('\033[37m', '\033[47m')
+    BLACK = _ColorCodes("\033[30m", "\033[40m")
+    RED = _ColorCodes("\033[31m", "\033[41m")
+    GREEN = _ColorCodes("\033[32m", "\033[42m")
+    YELLOW = _ColorCodes("\033[33m", "\033[43m")
+    BLUE = _ColorCodes("\033[34m", "\033[44m")
+    MAGENTA = _ColorCodes("\033[35m", "\033[45m")
+    CYAN = _ColorCodes("\033[36m", "\033[46m")
+    WHITE = _ColorCodes("\033[37m", "\033[47m")
 
 
 def colored_text(text: str, color: ANSIColor):
-    return f'{color.text_color_code}{text}'
+    return f"{color.text_color_code}{text}"
 
 
 def colored_background(text: str, color: ANSIColor):
-    return f'{color.background_color_code}{text}'
+    return f"{color.background_color_code}{text}"
--- ascii_game/test_print_map.py
+++ ascii_game/test_print_map.py
@@ -1,92 +1,148 @@
 from ascii_game.colored_text import ANSIColor, colored_text
 
-DEFAULT_TILE_0 = '░░'  # Alt+0183
-DEFAULT_TILE_1 = '··'  # Alt+0183
-DEFAULT_TILE_2 = '••'  # Alt+0149
-DEFAULT_TILE_3 = '🦔'  # Alt+0149
-DEFAULT_TILE_4 = '::'  # Alt+0149
-DEFAULT_TILE_5 = '■'  # Alt+0149
-DEFAULT_TILE_6 = '▬'  # Alt+0149
+DEFAULT_TILE_0 = "░░"  # Alt+0183
+DEFAULT_TILE_1 = "··"  # Alt+0183
+DEFAULT_TILE_2 = "••"  # Alt+0149
+DEFAULT_TILE_3 = "🦔"  # Alt+0149
+DEFAULT_TILE_4 = "::"  # Alt+0149
+DEFAULT_TILE_5 = "■"  # Alt+0149
+DEFAULT_TILE_6 = "▬"  # Alt+0149
 
 
 class Vector1:
     def __init__(self, x: float = 0.0):
         self.x = x
 
-    def get_float_x(self): return self.x
-    def get_int_x(self): return int(self.x)
+    def get_float_x(self):
+        return self.x
+
+    def get_int_x(self):
+        return int(self.x)
 
 
-class Vector2(Vector1):
+class Vector2:
     def __init__(self, x: float = 0.0, y: float = 0.0):
-        super().__init__(x)
+        self.x = x
         self.y = y
 
-    def get_float_y(self): return self.y
-    def get_int_y(self): return int(self.y)
+    def get_float_x(self):
+        return self.x
+
+    def get_int_x(self):
+        return int(self.x)
+
+    def get_float_y(self):
+        return self.y
+
+    def get_int_y(self):
+        return int(self.y)
 
 
-class Vector3(Vector2):
+class Vector3:
     def __init__(self, x: float = 0.0, y: float = 0.0, z: float = 0.0):
-        super().__init__(x, y)
+        self.x = x
+        self.y = y
         self.z = z
 
-    def get_float_z(self): return self.z
-    def get_int_z(self): return int(self.z)
+    def get_float_x(self):
+        return self.x
+
+    def get_int_x(self):
+        return int(self.x)
+
+    def get_float_y(self):
+        return self.y
+
+    def get_int_y(self):
+        return int(self.y)
+
+    def get_float_z(self):
+        return self.z
+
+    def get_int_z(self):
+        return int(self.z)
 
 
-class Vector4(Vector3):
+class Vector4:
     def __init__(self, x: float = 0.0, y: float = 0.0, z: float = 0.0, w: float = 0.0):
-        super().__init__(x, y, z)
+        self.x = x
+        self.y = y
+        self.z = z
         self.w = w
 
-    def get_float_w(self): return self.w
-    def get_int_w(self): return int(self.w)
+    def get_float_x(self):
+        return self.x
+
+    def get_int_x(self):
+        return int(self.x)
+
+    def get_float_y(self):
+        return self.y
+
+    def get_int_y(self):
+        return int(self.y)
+
+    def get_float_z(self):
+        return self.z
+
+    def get_int_z(self):
+        return int(self.z)
+
+    def get_float_w(self):
+        return self.w
+
+    def get_int_w(self):
+        return int(self.w)
 
 
-class PhysicalObject:
+class PhysicalComponent:
     def __init__(self, map_range: Vector3 = Vector3(1, 1, 1)):
         self.map_range = map_range
 
 
-class GraphicObject:
-    def __init__(self, texture: str = DEFAULT_TILE_0, color_modifier: str = ''):
+class GraphicComponent:
+    def __init__(self, texture: str = DEFAULT_TILE_0, color_modifier: str = ""):
         self.texture = texture
         self.color_modifier = color_modifier  # TODO class ColorModifier
 
 
 class GameObject:
     def __init__(
-            self,
-            physicalObject: PhysicalObject = PhysicalObject(),
-            graphicObject: GraphicObject = GraphicObject()
+        self,
+        physicalObject: PhysicalComponent = PhysicalComponent(),
+        graphicObject: GraphicComponent = GraphicComponent(),
     ):
         self.physicalObject = physicalObject
         self.graphicObject = graphicObject
+
+
 #         TODO class collisionObject
 
 
 class LogicLevelMap:  # TODO достаточно отражается сущьность обхекта?
     def __init__(
-            self,
-            map_range: Vector3 = Vector3(16, 16, 16),
-            default_tile: GameObject = GameObject(
-                PhysicalObject(), GraphicObject()
-            ),
-            map_default_position: Vector3 = Vector3(8, 8, 4),
-            radius_tile: Vector1 = Vector1(1),  # TODO куда этот параметр?
+        self,
+        map_range: Vector3 = Vector3(16, 16, 16),
+        default_tile: GameObject = GameObject(PhysicalComponent(), GraphicComponent()),
+        map_default_position: Vector3 = Vector3(8, 8, 4),
+        radius_view_tile: Vector1 = Vector1(1),  # TODO куда этот параметр?
     ):
-        # TODO переписать на 3 измерения
-        self.map = [[[default_tile for x in range(map_range.get_int_x())] for y in range(map_range.get_int_y())] for z in range(map_range.get_int_z())]
+        self.map = [
+            [
+                [default_tile for x in range(map_range.get_int_x())]
+                for y in range(map_range.get_int_y())
+            ]
+            for z in range(map_range.get_int_z())
+        ]
         self.map_default_position = map_default_position
-        self.radius_tile = radius_tile
+        self.radius_tile = radius_view_tile
 
 
 class Camera:
     def __init__(
-            self,
-            vision: Vector3 = Vector3(8, 8, 1),
-            position: Vector3 = Vector3(0, 0, 0),  # TODO куда этот параметр?
+        self,
+        vision: Vector3 = Vector3(8, 8, 1),
+        position: Vector3 = Vector3(0, 0, 0),  # TODO куда этот параметр?
     ):
         self.vision = vision
         self.position = position
@@ -94,9 +150,7 @@ class Camera:
 
 class Scene:
     def __init__(
-            self,
-            logicLevelMap: LogicLevelMap = LogicLevelMap(),
-            camera: Camera = Camera()
+        self, logicLevelMap: LogicLevelMap = LogicLevelMap(), camera: Camera = Camera()
     ):
         self.logicLevelMap = logicLevelMap
         self.camera = camera
@@ -116,8 +170,8 @@ class Scene:
         x_end = self.camera.vision.get_int_x()
 
         # for z in range(z_start, z_start + z_end):
-        for z in range(z_start + z_end, z_start + z_end):
-            for rz in range(r):
+        for z in range(z_start + z_end, z_start + z_end + 1):
+            for rz in range(r):  # TODO Не нужно. Скорее всего
                 for y in range(y_start, y_start + y_end):
                     for ry in range(r):
                         for x in range(x_start, x_start + x_end):
@@ -150,33 +204,48 @@ class Scene:
                             raw_render_map[z - z_start][y - y_start].append([])
                             for rx in range(r):
 
-                                is_out_of_range = False
-                                if z < len(map):
-                                    if y < len(map[z]):
-                                        if x < len(map[z][y]):
-                                            is_out_of_range = True
-
-                                if is_out_of_range:
+                                if self.camera_is_out_of_map_range(map, x, y, z):
                                     if y == y_start:
                                         if x != x_start:
-                                            texture = str(x - 1).rjust(2, '_')
-                                            game_object = GameObject(PhysicalObject(), GraphicObject(texture=texture))
-                                            raw_render_map[z - z_start][y - y_start][x - x_start] = game_object
+                                            texture = str(x - 1).rjust(2, "_")
+                                            game_object = GameObject(
+                                                PhysicalComponent(),
+                                                GraphicComponent(texture=texture),
+                                            )
+                                            raw_render_map[z - z_start][y - y_start][
+                                                x - x_start
+                                            ] = game_object
                                         else:
-                                            texture = '  '
-                                            game_object = GameObject(PhysicalObject(), GraphicObject(texture=texture))
-                                            raw_render_map[z - z_start][y - y_start][x - x_start] = game_object
+                                            texture = "  "
+                                            game_object = GameObject(
+                                                PhysicalComponent(),
+                                                GraphicComponent(texture=texture),
+                                            )
+                                            raw_render_map[z - z_start][y - y_start][
+                                                x - x_start
+                                            ] = game_object
                                     if y != y_start:
                                         if x == x_start:
-                                            texture = str(y - 1).rjust(2, '_')
-                                            game_object = GameObject(PhysicalObject(), GraphicObject(texture=texture))
-                                            raw_render_map[z - z_start][y - y_start][x - x_start] = game_object
+                                            texture = str(y - 1).rjust(2, "_")
+                                            game_object = GameObject(
+                                                PhysicalComponent(),
+                                                GraphicComponent(texture=texture),
+                                            )
+                                            raw_render_map[z - z_start][y - y_start][
+                                                x - x_start
+                                            ] = game_object
                                         else:
-                                            raw_render_map[z - z_start][y - y_start][x - x_start] = map[z][y][x]
+                                            raw_render_map[z - z_start][y - y_start][
+                                                x - x_start
+                                            ] = map[z][y][x]
                                 else:
-                                    texture = ''
-                                    game_object = GameObject(PhysicalObject(), GraphicObject(texture=texture))
-                                    raw_render_map[z - z_start][y - y_start][x - x_start] = game_object
+                                    texture = ""
+                                    game_object = GameObject(
+                                        PhysicalComponent(), GraphicComponent(texture=texture)
+                                    )
+                                    raw_render_map[z - z_start][y - y_start][
+                                        x - x_start
+                                    ] = game_object
 
         for z in range(1, len(raw_render_map)):
             for rz in range(r):
@@ -184,31 +253,29 @@ class Scene:
                     for ry in range(r):
                         for x in range(1, len(raw_render_map[z][y])):
                             for rx in range(r):
-                                is_out_of_range = False
+                                camera_is_out_of_map_range = False
                                 if y < len(map):
                                     if x < len(map[y]):
-                                        is_out_of_range = True
+                                        camera_is_out_of_map_range = True
 
-                                if is_out_of_range:
+                                if camera_is_out_of_map_range:
                                     raw_render_map[z][y][x] = map[z][y][x]
 
         return raw_render_map
 
+    @staticmethod
+    def camera_is_out_of_map_range(map, x, y, z):
+        camera_is_out_of_map_range = False
+        if z < len(map):
+            if y < len(map[z]):
+                if x < len(map[z][y]):
+                    camera_is_out_of_map_range = True
+        return camera_is_out_of_map_range
+
 
 def main():
     scene = Scene()
-    # scene.rendering()
-    # rendering_2d_matrix(scene.info_rendering())
     rendering_3d_matrix(scene.info_rendering())
-    # map = [[DEFAULT_TILE_4 for x in range(8)] for y in range(8)]
-    # print_rainbow_2d_matrix(map)
-
-
-def print_2d_matrix(matrix: list[list[str]]):
-    for i in matrix:
-        for j in i:
-            print(colored_text(j, ANSIColor.GREEN), end=" ")
-        print()
 
 
 def rendering_2d_matrix(matrix: list[list[GameObject]]):
@@ -226,27 +293,5 @@ def rendering_3d_matrix(matrix: list[list[list[GameObject]]]):
             print()
 
 
-def print_rainbow_2d_matrix(matrix: list[list[str]]):
-    color = [
-        ANSIColor.BLACK,
-        ANSIColor.RED,
-        ANSIColor.GREEN,
-        ANSIColor.YELLOW,
-        ANSIColor.BLUE,
-        ANSIColor.MAGENTA,
-        ANSIColor.CYAN,
-        ANSIColor.WHITE,
-    ]
-    r = 1
-    for y in range(len(matrix)):
-        for ry in range(r):
-            for x in range(len(matrix[y])):
-                for rx in range(r):
-                    print(colored_text(matrix[y][x], color[y - x]), end=" ")
-                    # print(colored_text(matrix[y][x], ANSIColor.WHITE), end=" ")
-                    # print(matrix[y][x], end=" ")
-            print()
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
