bbb3cdb3d687ca199661b72b2625098e545137cc
Faunu <Tegucigalpa>
2021-11-24 00:28:44 +0300

    refactor Scene.get_camera

--- ascii_game/scene.py
+++ ascii_game/scene.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import cast
 
 from ascii_game.object.game_object import GameObject
 from ascii_game.render.camera import Camera
@@ -13,14 +14,9 @@ class Scene:
         self.objects = [Camera()]  # transform x:0 y:0 z:0
         self.camera_id = 0
 
-    def get_camera(self) -> Camera | None:
+    def get_camera(self) -> Camera:
         camera = self.objects[self.camera_id]
-        if not isinstance(camera, Camera):
-            try:
-                camera = next(filter(lambda camera: isinstance(camera, Camera), self.objects))
-            except StopIteration:
-                return None
-        return camera
+        return cast(Camera, camera)
 
     def add_object(self, game_object: GameObject):
         self.objects.append(game_object)
