9a33882abba4bda00322639f161f2d3295fcd6fa
faunus_C <FaunusCarmel@yandex.ru>
2022-04-28 00:20:59 +0300

    bag-fix bearlibterminal rendering

--- nagoya/src/nagoya/render/bearlibterminal_renderer.py
+++ nagoya/src/nagoya/render/bearlibterminal_renderer.py
@@ -7,7 +7,10 @@ def render_to_terminal(buffer: Buffer) -> None:
     for y in range(len(buffer.rendered_tiles)):
         for x in range(len(buffer.rendered_tiles[y])):
             tile = buffer.rendered_tiles[y][x]
+            object_texture = tile.object_texture.object_id
+            if tile.object_texture.object_id == "":
+                object_texture = " "
             terminal.bkcolor(tile.background_color.get_rgba_hex())
             terminal.color(tile.object_color.get_rgba_hex())
-            terminal.put(x, buffer.viewport.y - 1 - y, tile.object_texture.object_id)
+            terminal.put(x, buffer.viewport.y - 1 - y, object_texture)
     terminal.refresh()
