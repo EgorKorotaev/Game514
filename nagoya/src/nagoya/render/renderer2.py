105b8f0c136ec08aed0e26dd77bb93a9dfc821ab
faunus_C <FaunusCarmel@yandex.ru>
2022-05-06 01:44:23 +0300

    add isometry render

---
+++ nagoya/src/nagoya/render/renderer2.py
@@ -0,0 +1,126 @@
+import time
+from dataclasses import dataclass
+from typing import cast
+
+from nagoya.component.camera_component import CameraComponent
+from nagoya.component.renderer_component import RendererComponent
+from nagoya.drawing_in_console.rendered_tile import RenderedTile
+from nagoya.object.game_object import GameObject
+from nagoya.primitive.vector import Vector3
+from nagoya.scene import Scene
+
+
+@dataclass
+class Buffer2:
+    tiles: list[list[list[list[GameObject]]]]
+    rendered_tiles: list[list[RenderedTile]]
+
+    def __init__(self, camera: GameObject):
+        self.camera_position = camera.transform.position
+        camera_component = cast(CameraComponent, camera.get_component(CameraComponent))
+        self.viewport = camera_component.viewport
+        self.center = camera_component.center
+        self.tiles = [[[[] for z in range(self.viewport.z)] for x in range(self.viewport.x)] for y in range(self.viewport.y)]
+
+        self.default_rendered_tile = camera_component.default_rendered_shader.render(underlying_tile=None)
+        self.rendered_tiles = [
+            [self.default_rendered_tile for x in range(self.viewport.x * 2 + self.viewport.y + 2)] for y in range(self.viewport.y + self.viewport.z + 3)
+        ]
+        self.offset_y = self.viewport.z
+        self.offset_x = self.viewport.y
+
+    def add_game_object(self, game_object: GameObject):
+        x = game_object.transform.position.x - self.camera_position.x
+        y = game_object.transform.position.y - self.camera_position.y
+        z = game_object.transform.position.z - self.camera_position.z
+
+        tile_is_in_viewport = (
+            z >= 0
+            and y >= 0
+            and x >= 0
+            and z < self.viewport.z
+            and y < self.viewport.y
+            and x < self.viewport.x
+        )
+        if tile_is_in_viewport:
+            self.tiles[y][x][z].append(game_object)
+
+    def draw_tiles(self):
+        for y in range(len(self.tiles) - 1, 0, -1):
+        # for y in range(len(self.tiles)):
+            for x in range(len(self.tiles[y])):
+        #     for x in range(len(self.tiles[y]) - 1, 0, -1):
+                for z in range(len(self.tiles[y][x])):
+                    if not len(self.tiles[y][x][z]):
+                        continue
+
+                    self.tiles[y][x][z].sort(
+                        key=lambda i: (i.get_component(RendererComponent).priority)
+                    )
+
+                    # _y = y + z + self.offset_y
+                    # _x = x * 2 - y + self.offset_x
+                    _y = y + z - self.offset_y
+                    _x = x * 2 + y - self.offset_x
+
+                    rendered_tile_0 = self.rendered_tiles[_y][_x]
+                    rendered_tile_1 = self.rendered_tiles[_y + 1][_x]
+                    rendered_tile_2 = self.rendered_tiles[_y + 1][_x + 1]
+                    rendered_tile_3 = self.rendered_tiles[_y + 1][_x + 2]
+                    rendered_tile_4 = self.rendered_tiles[_y][_x + 2]
+                    rendered_tile_5 = self.rendered_tiles[_y][_x + 1]
+
+
+                    for i in range(len(self.tiles[y][x][z])):
+                        rendered_object = self.tiles[y][x][z][i]
+                        rendered_tile_0 = cast(RendererComponent, rendered_object.get_component(RendererComponent)).draw(
+                            rendered_tile_0, Vector3(x=0, y=1, z=0)
+                        )
+                        rendered_tile_1 = cast(RendererComponent, rendered_object.get_component(RendererComponent)).draw(
+                            rendered_tile_1, Vector3(x=0, y=0, z=0)
+                        )
+                        rendered_tile_2 = cast(RendererComponent, rendered_object.get_component(RendererComponent)).draw(
+                            rendered_tile_2, Vector3(x=1, y=0, z=0)
+                        )
+                        rendered_tile_3 = cast(RendererComponent, rendered_object.get_component(RendererComponent)).draw(
+                            rendered_tile_3, Vector3(x=2, y=0, z=0)
+                        )
+                        rendered_tile_4 = cast(RendererComponent, rendered_object.get_component(RendererComponent)).draw(
+                            rendered_tile_4, Vector3(x=2, y=1, z=0)
+                        )
+                        rendered_tile_5 = cast(RendererComponent, rendered_object.get_component(RendererComponent)).draw(
+                            rendered_tile_5, Vector3(x=1, y=1, z=0)
+                        )
+
+                    self.rendered_tiles[_y][_x] = rendered_tile_0
+                    self.rendered_tiles[_y + 1][_x] = rendered_tile_1
+                    self.rendered_tiles[_y + 1][_x + 1] = rendered_tile_2
+                    self.rendered_tiles[_y + 1][_x + 2] = rendered_tile_3
+                    self.rendered_tiles[_y][_x + 2] = rendered_tile_4
+                    self.rendered_tiles[_y][_x + 1] = rendered_tile_5
+
+    def print(self):
+        for y in reversed(range(len(self.rendered_tiles))):
+            for x in range(len(self.rendered_tiles[y])):
+                printed_symbol = self.rendered_tiles[y][x].get_printed_symbol()
+                print(printed_symbol, end="")
+            print()
+
+
+def render_scene2(scene: Scene) -> Buffer2:
+    camera = scene.get_camera()
+    buffer = Buffer2(camera)
+
+    game_objects = scene.game_objects()
+    drawable_game_objects = get_drawable_game_objects(game_objects)
+
+    for game_object in drawable_game_objects:
+        buffer.add_game_object(game_object)
+
+    buffer.draw_tiles()
+
+    return buffer
+
+
+def get_drawable_game_objects(game_objects: list[GameObject]) -> list[GameObject]:
+    return [game_object for game_object in game_objects if game_object.get_component(RendererComponent) is not None]
---
+++ nagoya/src/nagoya/shader/simple_shader2.py
@@ -0,0 +1,138 @@
+from nagoya.drawing_in_console.rendered_tile import RenderedTile
+from nagoya.primitive import ColorA
+from nagoya.primitive.vector import Vector3
+from nagoya.render.texture import ObjectTexture
+
+from .shader import Shader
+from .shader_visitor import ShaderVisitor
+
+
+class SimpleShader2(Shader):
+    def __init__(self, background_color: ColorA, object_color: ColorA, object_texture: ObjectTexture):
+        self.background_color_x = background_color
+        self.background_color_y = background_color.lighten(0.2)
+        self.background_color_z = background_color.darken(0.2)
+        self.object_color = object_color
+        self.object_texture = object_texture
+        self.sub_texture_1 = ObjectTexture('◤')
+        self.sub_texture_2 = ObjectTexture('◢')
+
+    def render(
+            self, underlying_tile: RenderedTile | None, position_relative_to_camera: Vector3 | None = None
+    ) -> RenderedTile:
+        this_tile = RenderedTile(self.background_color_x, self.object_color, self.object_texture)
+
+        # if underlying_tile is None:
+        #     return this_tile
+
+        # self.adjust_color_to_height(this_tile, position_relative_to_camera.z)
+        tile_mixed = self.mix_tiles(underlying_tile, this_tile, position_relative_to_camera)
+
+        return tile_mixed
+
+    def mix_tiles(self, underlying_tile: RenderedTile | None, tile: RenderedTile | None,
+                  config: Vector3) -> RenderedTile | None:
+        if config.x == 0 and config.y == 0:  # 1
+            background_color = self.background_color_x
+            object_color = ColorA(a=0)
+            object_texture = self.sub_texture_1
+
+        elif config.x == 0 and config.y == 1:  # 2
+            background_color = self.background_color_y
+            object_color = ColorA(a=0)
+            object_texture = ObjectTexture()
+
+        elif config.x == 1 and config.y == 1:  # 3
+            background_color = self.background_color_y
+            object_color = ColorA(a=0)
+            object_texture = ObjectTexture()
+
+        elif config.x == 2 and config.y == 1:  # 4
+            background_color = ColorA(a=0)
+            object_color = self.background_color_z
+            object_texture = self.sub_texture_1
+
+        elif config.x == 2 and config.y == 0:  # 5
+            background_color = self.background_color_x
+            object_color = self.background_color_z
+            object_texture = self.sub_texture_2
+
+        elif config.x == 1 and config.y == 0:  # 6
+            background_color = self.background_color_x
+            object_color = ColorA(a=0)
+            object_texture = ObjectTexture()
+
+        else:
+            background_color = ColorA(a=0)
+            object_color = ColorA(a=0)
+            object_texture = ObjectTexture()
+
+        if underlying_tile is not None:
+            if config.x == 0 and config.y == 0:  # 1
+                if underlying_tile.object_texture != self.sub_texture_1:
+                    background_color = underlying_tile.background_color + background_color
+                    object_color = underlying_tile.background_color
+                else:
+                    # background_color = underlying_tile.object_color + background_color
+                    object_color = underlying_tile.object_color
+                    object_texture = underlying_tile.object_texture
+                    background_color = underlying_tile.background_color + background_color
+
+            elif config.x == 0 and config.y == 1:  # 2
+                object_color = underlying_tile.object_color + background_color
+                background_color = underlying_tile.background_color + background_color
+                object_texture = underlying_tile.object_texture
+
+            elif config.x == 1 and config.y == 1:  # 3
+                background_color = underlying_tile.background_color + background_color
+                if underlying_tile.object_texture != ObjectTexture():
+                    object_color = underlying_tile.object_color + object_color
+                    object_texture = underlying_tile.object_texture
+
+            elif config.x == 2 and config.y == 1:  # 4
+                object_color = underlying_tile.object_color + object_color
+                if underlying_tile.object_texture != self.sub_texture_1:
+                    background_color = underlying_tile.object_color + background_color
+                else:
+                    background_color = underlying_tile.background_color + background_color
+
+            elif config.x == 2 and config.y == 0:  # 5
+                # object_color = underlying_tile.object_color + object_color
+                if underlying_tile.object_texture == self.sub_texture_2:
+                    background_color = underlying_tile.background_color + background_color
+                    object_texture = underlying_tile.object_texture
+                    object_color = underlying_tile.object_color + object_color
+                else:
+                    object_color = underlying_tile.background_color + object_color
+                    background_color = underlying_tile.background_color + background_color
+
+            elif config.x == 1 and config.y == 0:  # 6
+                background_color = underlying_tile.background_color + background_color
+                if underlying_tile.object_texture != self.sub_texture_1:
+                    object_color = underlying_tile.object_color + object_color
+                    object_texture = underlying_tile.object_texture
+
+            # if underlying_tile.object_texture == self.sub_texture_1 or underlying_tile.object_texture == self.sub_texture_2:
+            #     pass
+
+        rendered_tile = RenderedTile(
+            background_color=background_color,
+            object_color=object_color,
+            object_texture=object_texture
+        )
+
+        return rendered_tile
+
+    def adjust_color_to_height(self, tile: RenderedTile, config: Vector3):
+        pass
+
+    def accept(self, visitor: ShaderVisitor):
+        return visitor.visit_simple(self)
+
+    @staticmethod
+    def create_simple_shader_2(
+            background_color: ColorA = ColorA(),
+            object_color: ColorA = ColorA(),
+            object_texture: ObjectTexture = ObjectTexture(),
+    ) -> "SimpleShader2":
+        return SimpleShader2(background_color, object_color, object_texture)
